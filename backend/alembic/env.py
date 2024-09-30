1bf17ca3f81bd907608c65acda3476306ea6df09
Mükail Askerov <90192553+AskMikk@users.noreply.github.com>
2024-09-30 15:49:33 +0300

    Fixed

--- backend/alembic/env.py
+++ backend/alembic/env.py
@@ -15,15 +15,36 @@ fileConfig(config.config_file_name)
 target_metadata = Base.metadata
 
 def run_migrations_offline():
-    url = config.get_main_option("sqlalchemy.url")
-    context.configure(url=url, target_metadata=target_metadata, literal_binds=True, dialect_opts={"paramstyle": "named"})
+    url = os.getenv('DATABASE_URL')
+    if not url:
+        raise ValueError("DATABASE_URL не установлена в переменных окружения")
+    context.configure(
+        url=url,
+        target_metadata=target_metadata,
+        literal_binds=True,
+        dialect_opts={"paramstyle": "named"},
+    )
     with context.begin_transaction():
         context.run_migrations()
 
 def run_migrations_online():
-    connectable = engine_from_config(config.get_section(config.config_ini_section), prefix="sqlalchemy.", poolclass=pool.NullPool)
+    url = os.getenv('DATABASE_URL')
+    if not url:
+        raise ValueError("DATABASE_URL не установлена в переменных окружения")
+
+    connectable = engine_from_config(
+        {
+            'sqlalchemy.url': url
+        },
+        prefix='sqlalchemy.',
+        poolclass=pool.NullPool
+    )
+
     with connectable.connect() as connection:
-        context.configure(connection=connection, target_metadata=target_metadata)
+        context.configure(
+            connection=connection,
+            target_metadata=target_metadata
+        )
         with context.begin_transaction():
             context.run_migrations()
 
